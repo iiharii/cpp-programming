7db9f17eacf183988e607be67aab16d7832adbda
hari <hari@DESKTOP-Q59FLKF.localdomain>
2026-09-18 21:48:44 +0900

    commit

--- w03_1/types-char.cpp
+++ w03_1/types-char.cpp
@@ -5,4 +5,10 @@ int main(){
     cout << "print \"a\\\"b\" :" << "a\"b" << endl;
     cout << "print \'\\\' :" << '\'' << endl;
     cout << "print \"a\\\\n\\n\" :" << "a\\n\n" << endl;
+
+    char ch = 97;
+    cout << "print 97 as a char: " << ch << endl;
+
+    ch = '\141';
+    cout << "print \'141\' as a char: " << ch << endl;
 }
