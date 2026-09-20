96a647db0a7ed41fa126113fc263fa6b17ae4694
hari <hari@BOOK-8342JOI8ID.localdomain>
2026-09-20 20:27:34 +0900

    commit

--- HW02/intlimit2.cpp
+++ HW02/intlimit2.cpp
@@ -10,7 +10,7 @@ int main(){
     while(true){
         cin >> n;
         if(n>0){
-            mul = (mul * n) % 10;
+            mul = (mul * (n % 10) % 10;  // n % 10을 통해 int 범위 초과 문제 해결
         }
         else if(n==0){
             cout << mul << endl;
